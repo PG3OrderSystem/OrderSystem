f098018097c14b7a85d65c14e9859bc7094a0565
c5221 <c5221@oic.jp>
2026-07-03 13:43:05 +0900

    基本ファイルの作成

--- OrderSystem/Models/ModelClass.cs
+++ OrderSystem/Models/ModelClass.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OrderSystem
+namespace OrderSystem.Models
 {
-    class TestClass
+    class ModelClass
     {
-        //aaaaaaaaaaaa
     }
 }
